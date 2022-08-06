d85b450152875563e10f5534a2b510d4137034ed
Taylor Hopper <taylorhopper@outlook.com>
2022-08-06 15:20:15 -0700

    Add some simple command lines for disassembly

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,36 @@
 module Main where
 
-import Lib
+import           Data.Semigroup      ((<>))
+import           Options.Applicative
+
+data DisassemblerArgs = DisassemblerArgs
+  { file   :: FilePath
+  , offset :: Int
+  , length :: Int
+  }
+
+disassemblerArgsParser :: Parser DisassemblerArgs
+disassemblerArgsParser =
+  DisassemblerArgs <$>
+  strOption (long "file" <> metavar "FILE" <> help "The file to disassemble") <*>
+  option
+    auto
+    (long "offset" <> short 'o' <> help "The offset into the assembly file" <>
+     metavar "INT" <>
+     value 1) <*>
+  option
+    auto
+    (long "length" <> short 'l' <> help "The number of instructions to read" <>
+     metavar "INT" <>
+     value 100)
+
+disassemble :: DisassemblerArgs -> IO ()
+disassemble args = putStrLn $ file args
 
 main :: IO ()
-main = someFunc
+main = disassemble =<< execParser opts
+  where
+    opts =
+      info
+        (disassemblerArgsParser <**> helper)
+        (fullDesc <> progDesc "Disassemble 8080 assembly for FILE")
--- src/Lib.hs
+++
@@ -1,8 +0,0 @@
-module Lib
-    ( someFunc
-    ) where
-
-import Instruction
-
-someFunc :: IO ()
-someFunc = print (toInstruction 0x01)
