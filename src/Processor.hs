fc5217ede47d0546a972e8f2ba7ea66cfb014e3a
Taylor Hopper <taylorhopper@outlook.com>
2022-08-07 16:27:02 -0700

    Convert processor to use a State monad

--- src/Processor.hs
+++ src/Processor.hs
@@ -1,5 +1,6 @@
 module Processor where
 
+import           Control.Monad.State
 import           Data.Bits
 import           Data.IntMap
 import           Data.Maybe
@@ -32,8 +33,11 @@ data Processor = Processor
   , memory    :: IntMap Word8
   }
 
-readRegister8 M processor = readMemory8 (readRegister16 HL processor) processor
-readRegister8 reg processor = toReader reg . registers $ processor
+readRegister8 :: Register8 -> State Processor Word8
+readRegister8 M = do
+  addr <- readRegister16 HL
+  readMemory8 addr
+readRegister8 reg = gets $ toReader reg . registers
 
 toReader A = a
 toReader B = b
@@ -43,16 +47,18 @@ toReader E = e
 toReader H = h
 toReader L = l
 
+readRegister16 :: Register16 -> State Processor Word16
 readRegister16 BC = readRegister16' B C
 readRegister16 DE = readRegister16' D E
 readRegister16 HL = readRegister16' H L
-readRegister16 PC = pc . registers
-readRegister16 SP = sp . registers
+readRegister16 PC = gets $ pc . registers
+readRegister16 SP = gets $ sp . registers
 
-readRegister16' high low processor = to16 highVal lowVal
-  where
-    highVal = readRegister8 high processor
-    lowVal = readRegister8 low processor
+readRegister16' :: Register8 -> Register8 -> State Processor Word16
+readRegister16' high low = do
+  highVal <- readRegister8 high
+  lowVal <- readRegister8 low
+  return $ to16 highVal lowVal
 
 to16 :: Word8 -> Word8 -> Word16
 to16 high low = shift (fromIntegral high) 8 .|. fromIntegral low
@@ -60,28 +66,36 @@ to16 high low = shift (fromIntegral high) 8 .|. fromIntegral low
 from16 :: Word16 -> (Word8, Word8)
 from16 value = (fromIntegral $ shift value (-8), fromIntegral value)
 
-readMemory8 addr = fromJust . Data.IntMap.lookup (fromIntegral addr) . memory
+readMemory8 :: Word16 -> State Processor Word8
+readMemory8 addr =
+  gets $ fromJust . Data.IntMap.lookup (fromIntegral addr) . memory
 
-readMemory16 addr processor = to16 high low
-  where
-    low = readMemory8 addr processor
-    high = readMemory8 (addr + 1) processor
+readMemory16 :: Word16 -> State Processor Word16
+readMemory16 addr = do
+  low <- readMemory8 addr
+  high <- readMemory8 (addr + 1)
+  return $ to16 high low
 
-readImmediate8 processor = (result, newProcessor)
-  where
-    counter = readRegister16 PC processor
-    result = readMemory8 counter processor
-    newProcessor = writeRegister16 PC (counter + 1) processor
+readImmediate8 :: State Processor Word8
+readImmediate8 = do
+  counter <- readRegister16 PC
+  result <- readMemory8 counter
+  writeRegister16 PC (counter + 1)
+  return result
 
-readImmediate16 processor = (to16 high low, newProcessor')
-  where
-    (low, newProcessor) = readImmediate8 processor
-    (high, newProcessor') = readImmediate8 newProcessor
+readImmediate16 :: State Processor Word16
+readImmediate16 = do
+  low <- readImmediate8
+  high <- readImmediate8
+  return $ to16 high low
 
-writeRegister8 M value processor =
-  writeMemory8 (readRegister16 HL processor) value processor
-writeRegister8 reg value processor =
-  processor {registers = toWriter reg value (registers processor)}
+writeRegister8 :: Register8 -> Word8 -> State Processor ()
+writeRegister8 M value = do
+  addr <- readRegister16 HL
+  writeMemory8 addr value
+writeRegister8 reg value =
+  modify $ \processor ->
+    processor {registers = toWriter reg value (registers processor)}
 
 toWriter A value registers = registers {a = value}
 toWriter B value registers = registers {b = value}
@@ -91,143 +105,165 @@ toWriter E value registers = registers {e = value}
 toWriter H value registers = registers {h = value}
 toWriter L value registers = registers {l = value}
 
-writeRegister16 BC value processor = writeRegister16' B C value processor
-writeRegister16 DE value processor = writeRegister16' D E value processor
-writeRegister16 HL value processor = writeRegister16' H L value processor
-writeRegister16 PC value processor =
-  processor {registers = (registers processor) {pc = value}}
-writeRegister16 SP value processor =
-  processor {registers = (registers processor) {sp = value}}
+writeRegister16 :: Register16 -> Word16 -> State Processor ()
+writeRegister16 BC value = writeRegister16' B C value
+writeRegister16 DE value = writeRegister16' D E value
+writeRegister16 HL value = writeRegister16' H L value
+writeRegister16 PC value =
+  modify $ \processor ->
+    processor {registers = (registers processor) {pc = value}}
+writeRegister16 SP value =
+  modify $ \processor ->
+    processor {registers = (registers processor) {sp = value}}
 
-writeRegister16' high low value =
-  writeRegister8 high highVal . writeRegister8 low lowVal
-  where
-    (highVal, lowVal) = from16 value
+writeRegister16' :: Register8 -> Register8 -> Word16 -> State Processor ()
+writeRegister16' high low value = do
+  let (highVal, lowVal) = from16 value
+  writeRegister8 high highVal
+  writeRegister8 low lowVal
 
-writeMemory8 addr value processor =
-  processor {memory = insert (fromIntegral addr) value (memory processor)}
+writeMemory8 :: Word16 -> Word8 -> State Processor ()
+writeMemory8 addr value =
+  modify $ \processor ->
+    processor {memory = insert (fromIntegral addr) value (memory processor)}
 
-writeMemory16 addr value processor = newProcessor'
-  where
-    (high, low) = from16 value
-    newProcessor = writeMemory8 addr low processor
-    newProcessor' = writeMemory8 (addr + 1) high newProcessor
+writeMemory16 :: Word16 -> Word16 -> State Processor ()
+writeMemory16 addr value = do
+  let (high, low) = from16 value
+  writeMemory8 addr low
+  writeMemory8 (addr + 1) high
 
-processBinaryArithmetic from op processor =
-  processor {flags = newFlags, registers = newRegisters}
-  where
-    left = fromIntegral $ readRegister8 A processor
-    right = fromIntegral $ readRegister8 from processor
-    result = left `op` right
-    newFlags = getArithmeticFlags result
-    newRegisters = (registers processor) {a = fromIntegral result}
+processRegisterBinaryArithmetic ::
+     Register8 -> (Word16 -> Word16 -> Word16) -> State Processor ()
+processRegisterBinaryArithmetic = processBinaryArithmetic . readRegister8
 
-processImmediateBinaryArithmetic op processor = newProcessor' {flags = newFlags}
-  where
-    left = readRegister8 A processor
-    (right, newProcessor) = readImmediate8 processor
-    result = fromIntegral left `op` fromIntegral right
-    newFlags = getArithmeticFlags result
-    newProcessor' = writeRegister8 A (fromIntegral result) newProcessor
+processImmediateBinaryArithmetic ::
+     (Word16 -> Word16 -> Word16) -> State Processor ()
+processImmediateBinaryArithmetic = processBinaryArithmetic readImmediate8
 
-withCarry op processor left right =
-  if cy . flags $ processor
-    then left `op` right `op` 1
-    else left `op` right
+processBinaryArithmetic ::
+     State Processor Word8 -> (Word16 -> Word16 -> Word16) -> State Processor ()
+processBinaryArithmetic getter op = do
+  left <- readRegister8 A
+  right <- getter
+  let result = fromIntegral left `op` fromIntegral right
+  updateArithmeticFlags result
+  writeRegister8 A (fromIntegral result)
+
+withCarry ::
+     (Word16 -> Word16 -> Word16)
+  -> State Processor (Word16 -> Word16 -> Word16)
+withCarry op = do
+  carry <- gets $ cy . flags
+  return $
+    if carry
+      then \left right -> left `op` right `op` 1
+      else op
 
 -- Force 16 bits here to easily check for the carry flag
-getArithmeticFlags :: Word16 -> Flags
-getArithmeticFlags result =
-  Flags
-    { z = result .&. 0xff == 0
-    , s = testBit result 7
-    , p = even . popCount $ result .&. 0xff
-    , cy = result > 0xff
-    , ac = False
-    }
+updateArithmeticFlags :: Word16 -> State Processor ()
+updateArithmeticFlags result =
+  modify $ \processor ->
+    processor
+      { flags =
+          (flags processor)
+            { z = result .&. 0xff == 0
+            , s = testBit result 7
+            , p = even . popCount $ result .&. 0xff
+            , cy = result > 0xff
+            }
+      }
 
-getIncDecFlags result flags =
-  flags
-    { z = result == 0
-    , s = testBit result 7
-    , p = even . popCount $ result .&. 0xff
-    }
+updateIncFlags :: Word8 -> State Processor ()
+updateIncFlags result =
+  modify $ \processor ->
+    processor
+      { flags =
+          (flags processor)
+            { z = result == 0
+            , s = testBit result 7
+            , p = even . popCount $ result .&. 0xff
+            }
+      }
 
-process :: Instruction -> Processor -> Processor
-process NOP processor = processor
-process (MOV to from) processor =
-  writeRegister8 to (readRegister8 from processor) processor
-process (MVI to) processor = writeRegister8 to value newProcessor
-  where
-    (value, newProcessor) = readImmediate8 processor
-process (LXI to) processor = writeRegister16 to addr newProcessor
-  where
-    (addr, newProcessor) = readImmediate16 processor
-process (STAX to) processor = writeMemory8 addr value processor
-  where
-    addr = readRegister16 to processor
-    value = readRegister8 A processor
-process (LDAX from) processor = writeRegister8 A value processor
-  where
-    value = readMemory8 (readRegister16 from processor) processor
-process STA processor = writeMemory8 addr value newProcessor
-  where
-    (addr, newProcessor) = readImmediate16 processor
-    value = readRegister8 A newProcessor
-process LDA processor = writeRegister8 A value newProcessor
-  where
-    (addr, newProcessor) = readImmediate16 processor
-    value = readMemory8 addr newProcessor
-process SHLD processor = writeMemory16 addr value newProcessor
-  where
-    value = readRegister16 HL processor
-    (addr, newProcessor) = readImmediate16 processor
-process LHLD processor = writeRegister16 HL value newProcessor
-  where
-    (addr, newProcessor) = readImmediate16 processor
-    value = readMemory16 addr newProcessor
-process XCHG processor = newProcessor'
-  where
-    oldDE = readRegister16 DE processor
-    oldHL = readRegister16 HL processor
-    newProcessor = writeRegister16 DE oldHL processor
-    newProcessor' = writeRegister16 HL oldDE newProcessor
-process (INR reg) processor = newProcessor {flags = newFlags}
-  where
-    newReg = readRegister8 reg processor + 1
-    newFlags = getIncDecFlags newReg (flags processor)
-    newProcessor = writeRegister8 reg newReg processor
-process (DCR reg) processor = newProcessor {flags = newFlags}
-  where
-    newReg = readRegister8 reg processor - 1
-    newFlags = getIncDecFlags newReg (flags processor)
-    newProcessor = writeRegister8 reg newReg processor
-process (INX reg) processor = writeRegister16 reg newReg processor
-  where
-    newReg = readRegister16 reg processor + 1
-process (DCX reg) processor = writeRegister16 reg newReg processor
-  where
-    newReg = readRegister16 reg processor - 1
-process (ADD from) processor = processBinaryArithmetic from (+) processor
-process (ADC from) processor =
-  processBinaryArithmetic from (withCarry (+) processor) processor
-process ADI processor = processImmediateBinaryArithmetic (+) processor
-process ACI processor =
-  processImmediateBinaryArithmetic (withCarry (+) processor) processor
-process (DAD from) processor =
-  processor {flags = newFlags, registers = newRegisters}
-  where
-    left = fromIntegral $ readRegister16 HL processor :: Word32
-    right = fromIntegral $ readRegister16 from processor :: Word32
-    result = left + right
-    newFlags = (flags processor) {cy = result > 0xffff}
-    high = shift (result .&. 0xff00) (-8)
-    low = result .&. 0xff
-    newRegisters =
-      (registers processor) {h = fromIntegral high, l = fromIntegral low}
-process (SUB from) processor = processBinaryArithmetic from (-) processor
-process (SBB from) processor =
-  processBinaryArithmetic from (withCarry (-) processor) processor
-process SUI processor = processImmediateBinaryArithmetic (-) processor
-process SBI processor =
-  processImmediateBinaryArithmetic (withCarry (-) processor) processor
+process :: Instruction -> State Processor ()
+process NOP = return ()
+process (MOV to from) = do
+  value <- readRegister8 from
+  writeRegister8 to value
+process (MVI to) = do
+  value <- readImmediate8
+  writeRegister8 to value
+process (LXI to) = do
+  addr <- readImmediate16
+  writeRegister16 to addr
+process (STAX to) = do
+  addr <- readRegister16 to
+  value <- readRegister8 A
+  writeMemory8 addr value
+process (LDAX from) = do
+  addr <- readRegister16 from
+  value <- readMemory8 addr
+  writeRegister8 A value
+process STA = do
+  addr <- readImmediate16
+  value <- readRegister8 A
+  writeMemory8 addr value
+process LDA = do
+  addr <- readImmediate16
+  value <- readMemory8 addr
+  writeRegister8 A value
+process SHLD = do
+  value <- readRegister16 HL
+  addr <- readImmediate16
+  writeMemory16 addr value
+process LHLD = do
+  addr <- readImmediate16
+  value <- readMemory16 addr
+  writeRegister16 HL value
+process XCHG = do
+  oldDE <- readRegister16 DE
+  oldHL <- readRegister16 HL
+  writeRegister16 DE oldHL
+  writeRegister16 HL oldDE
+process (INR reg) = do
+  value <- readRegister8 reg
+  let newValue = value + 1
+  updateIncFlags newValue
+  writeRegister8 reg newValue
+process (DCR reg) = do
+  value <- readRegister8 reg
+  let newValue = value - 1
+  updateIncFlags newValue
+  writeRegister8 reg newValue
+process (INX reg) = do
+  value <- readRegister16 reg
+  let newValue = value + 1
+  writeRegister16 reg newValue
+process (DCX reg) = do
+  value <- readRegister16 reg
+  let newValue = value - 1
+  writeRegister16 reg newValue
+process (ADD from) = processRegisterBinaryArithmetic from (+)
+process (ADC from) = do
+  op <- withCarry (+)
+  processRegisterBinaryArithmetic from op
+process ADI = processImmediateBinaryArithmetic (+)
+process ACI = do
+  op <- withCarry (+)
+  processImmediateBinaryArithmetic op
+process (DAD from) = do
+  left <- readRegister16 HL
+  right <- readRegister16 from
+  let result = fromIntegral left + fromIntegral right :: Word32
+  modify $ \processor ->
+    processor {flags = (flags processor) {cy = result > 0xffff}}
+  writeRegister16 HL (fromIntegral result)
+process (SUB from) = processRegisterBinaryArithmetic from (-)
+process (SBB from) = do
+  op <- withCarry (-)
+  processRegisterBinaryArithmetic from op
+process SUI = processImmediateBinaryArithmetic (-)
+process SBI = do
+  op <- withCarry (-)
+  processImmediateBinaryArithmetic op
