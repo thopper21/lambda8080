22a672726fe5935a237dab497052d9e8b96472ff
Taylor Hopper <taylorhopper@outlook.com>
2022-08-10 12:34:38 -0700

    Add support for a help message in the REPL

--- src/Repl.hs
+++ src/Repl.hs
@@ -11,19 +11,36 @@ newtype ErrorKind =
 
 data Action
   = Quit
+  | Help
   | Error ErrorKind
 
 parse :: String -> Action
 parse line =
   case line of
     "q" -> Quit
+    "h" -> Help
     _   -> Error (UnknownCommand line)
 
-eval :: Action -> IO ()
-eval Quit = return ()
-eval (Error (UnknownCommand command)) = do
-  putStrLn $ "Unknown command: \"" ++ command ++ "\""
+help :: IO ()
+help = do
+  putStrLn "Commands:"
+  putStrLn "\th\tDisplay this help message"
+  putStrLn "\tq\tQuit the debugger"
+
+error :: ErrorKind -> IO ()
+error (UnknownCommand command) = do
+  putStrLn $ "Unknown command: '" ++ command ++ "'"
+  help
+
+evalLoop :: IO () -> IO ()
+evalLoop action = do
+  action
   loop
+
+eval :: Action -> IO ()
+eval Quit              = return ()
+eval (Error errorKind) = evalLoop $ Repl.error errorKind
+eval Help              = evalLoop help
 
 prompt :: IO ()
 prompt = do
