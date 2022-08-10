a6056811fbf5d88442920fc28a3354f98f6d12b4
Taylor Hopper <taylorhopper@outlook.com>
2022-08-09 18:33:08 -0700

    Move Invaders to it's own module

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,10 @@ module Main where
 
 import           Control.Monad.State
 import           Data.ByteString     as BS (drop, readFile, unpack)
-import           Data.IntMap
-import           Data.Maybe
 import           Data.Semigroup      ((<>))
 import           Data.Word
 import           Disassembler
+import           Invaders
 import           Options.Applicative
 import           Processor
 
@@ -39,32 +38,11 @@ runDisassembler args = do
   let instructions = disassemble $ BS.unpack (BS.drop (offset args) assembly)
   mapM_ print $ take (count args) instructions
 
-stepN :: Int -> Processor SpaceInvaders -> Int -> IO (Processor SpaceInvaders)
+stepN :: Int -> Processor Invaders -> Int -> IO (Processor Invaders)
 stepN 0 processor _ = return processor
 stepN n processor i = do
   processor' <- execStateT step processor
   stepN (n - 1) processor' (i + 1)
-
--- Simple defaults for Space Invaders to run in attract mode
--- TODO: Implement Space Invaders shift register and move to invaders folder
-newtype SpaceInvaders = SpaceInvaders
-  { memory :: IntMap Word8
-  }
-
-instance Platform SpaceInvaders where
-  readMemory addr =
-    fromMaybe 0 . Data.IntMap.lookup (fromIntegral addr) . memory
-  writeMemory addr value invaders =
-    invaders {memory = insert (fromIntegral addr) value (memory invaders)}
-  readIn port _ =
-    case port of
-      0 -> return 0x01
-      _ -> return 0x00
-  writeOut port value _ = return ()
-
-initInvaders :: [Word8] -> SpaceInvaders
-initInvaders instructions =
-  SpaceInvaders {memory = fromAscList (zip [0 ..] instructions)}
 
 runProcessor args = do
   assembly <- BS.readFile $ file args
