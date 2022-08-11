fcb59ca4e30d98299972992fcdb4daf3a82206d4
Taylor Hopper <taylorhopper@outlook.com>
2022-08-10 18:15:10 -0700

    Improve usability

--- src/Repl.hs
+++ src/Repl.hs
@@ -2,10 +2,12 @@ module Repl
   ( runRepl
   ) where
 
+import           Control.Exception
 import           Control.Monad
 import           Control.Monad.State
-import           Data.ByteString     as BS (drop, readFile, unpack)
+import           Data.ByteString     as BS (ByteString, drop, readFile, unpack)
 import           Data.Maybe
+import           Data.Word
 import           GHC.IO.Exception    (IOErrorType (InvalidArgument))
 import           Invaders
 import           Processor
@@ -16,6 +18,7 @@ data ErrorKind
   = UnknownCommand String
   | InvalidArg String
                String
+  | MissingArg String
 
 data Action
   = Load String
@@ -38,19 +41,30 @@ parse line =
     ["h"] -> Help
     ["help"] -> Help
     ["l", file] -> Load file
+    ["l"] -> Error (MissingArg "FILE")
+    ["load"] -> Error (MissingArg "FILE")
     ["load", file] -> Load file
+    ["r"] -> Run 1000
     ["r", arg] ->
       case readMaybe arg of
         Just numLines -> Run numLines
         Nothing       -> Error (InvalidArg "non-negative integer" arg)
     _ -> Error (UnknownCommand line)
 
+loadWithAssembly :: [Word8] -> StateT ReplState IO ()
+loadWithAssembly assembly = do
+  let processor = initProcessor $ initInvaders assembly
+  modify $ \state -> state {game = Just processor}
+
+tryLoad :: String -> IO (Either IOException BS.ByteString)
+tryLoad = try . BS.readFile
+
 load :: String -> StateT ReplState IO ()
 load file = do
-  assembly <- liftIO $ BS.readFile file
-  let instructions = BS.unpack assembly
-  let processor = initProcessor $ initInvaders instructions
-  modify $ \state -> state {game = Just processor}
+  contents <- liftIO $ tryLoad file
+  case contents of
+    Left error     -> liftIO $ putStrLn $ "Invalid file name: " ++ show error
+    Right assembly -> loadWithAssembly $ BS.unpack assembly
 
 runGame :: Word -> Processor Invaders -> StateT ReplState IO ()
 runGame 0 currentGame = modify $ \s -> s {game = Just currentGame}
@@ -68,10 +82,10 @@ run n = do
 help :: IO ()
 help = do
   putStrLn "Commands:"
+  putStrLn "\tr(un) N[=1000]\tRun the program through N instructions"
   putStrLn "\tl(oad) FILE\tLoad an assembly file"
   putStrLn "\th(elp)\t\tDisplay this help message"
   putStrLn "\tq(uit)\t\tQuit the debugger"
-  putStrLn "\tr(un) N\t\tRun the program through N instructions"
 
 error :: ErrorKind -> IO ()
 error (UnknownCommand command) = do
@@ -79,6 +93,7 @@ error (UnknownCommand command) = do
   help
 error (InvalidArg expected actual) =
   putStrLn $ "Invalid argument. Expected " ++ expected ++ ", found " ++ actual
+error (MissingArg argName) = putStrLn $ "Missing argument: " ++ argName
 
 evalLoop :: StateT ReplState IO () -> StateT ReplState IO ()
 evalLoop action = do
