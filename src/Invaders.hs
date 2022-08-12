ddfdfca68a8496a1097c3b108a134aade564609d
Taylor Hopper <taylorhopper@outlook.com>
2022-08-11 18:02:31 -0700

    Implement Invaders Shift Register

--- src/Invaders.hs
+++ src/Invaders.hs
@@ -5,16 +5,40 @@ module Invaders
   ) where
 
 import           Control.Monad.State
-import           Data.IntMap                as IntMap
+import           Data.Bits
+import           Data.IntMap         as IntMap
 import           Data.Maybe
 import           Data.Word
 import           Processor
 
-newtype Invaders = Invaders
-  { memory :: IntMap Word8
+data ShiftRegister = ShiftRegister
+  { value  :: Word16
+  , offset :: Word8
+  }
+
+data Invaders = Invaders
+  { memory        :: IntMap Word8
+  , shiftRegister :: ShiftRegister
   }
 
 type InvadersState = StateT Invaders IO
+
+insertShift :: Word8 -> ShiftRegister -> ShiftRegister
+insertShift fromPort register = register {value = newValue}
+  where
+    newValue = shiftL (fromIntegral fromPort) 8 .|. shiftR (value register) 8
+
+setOffset :: Word8 -> ShiftRegister -> ShiftRegister
+-- Only bits 0, 1, and 2
+setOffset fromPort register = register {offset = fromPort .&. 0x7}
+
+getValue :: ShiftRegister -> Word8
+getValue register =
+  fromIntegral $ shiftR (value register) (8 - fromIntegral (offset register))
+
+modifyShiftRegister :: (ShiftRegister -> ShiftRegister) -> Invaders -> Invaders
+modifyShiftRegister setter prevState =
+  prevState {shiftRegister = setter $ shiftRegister prevState}
 
 readMemory :: Word16 -> InvadersState Word8
 readMemory addr =
@@ -26,13 +50,14 @@ writeMemory addr value =
     invaders {memory = insert (fromIntegral addr) value (memory invaders)}
 
 readIn :: Word8 -> InvadersState Word8
-readIn port =
-  case port of
-    0 -> return 0x01
-    _ -> return 0x00
+readIn 0 = return 0x01
+readIn 3 = gets $ getValue . shiftRegister
+readIn _ = return 0x00
 
 writeOut :: Word8 -> Word8 -> InvadersState ()
-writeOut port value = return ()
+writeOut 4 value = modify . modifyShiftRegister . insertShift $ value
+writeOut 2 value = modify . modifyShiftRegister . setOffset $ value
+writeOut _ _     = return ()
 
 connections :: Connections InvadersState
 connections =
@@ -45,4 +70,7 @@ connections =
 
 initInvaders :: [Word8] -> Invaders
 initInvaders instructions =
-  Invaders {memory = fromAscList (zip [0 ..] instructions)}
+  Invaders
+    { memory = fromAscList (zip [0 ..] instructions)
+    , shiftRegister = ShiftRegister {value = 0, offset = 0}
+    }
